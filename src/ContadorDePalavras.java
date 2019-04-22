7f739c4dc82bd08eb0fe446910a7cd6a8f77a557
Tozo <viniciusdelimatozo@hotmail.com>
2019-04-22 18:17:01 -0300

    Create ContadorDePalavras.java

---
+++ src/ContadorDePalavras.java
@@ -0,0 +1,32 @@
+// Utilize a implementação de árvore AVL para desenvolter um contador de palavras em arquivos txt.
+// O programa deve receber como entrada o nome de um diretório onde há arquivos ".txt"
+// e apresentar como saída um relatório que indica quantas vezes um termo foi encontrado em cada arquivo e o total de ocorrências.
+// Exemplo:
+//
+//Entre com um termo a ser pesquisado: "computador"
+//
+//Total de ocorrências de "computador": 6
+//
+//Arquivo "teste.txt": 3
+//
+//Arquivo "livro.txt": 2
+//
+//Arquivo "exame.txt": 1
+
+import java.util.Scanner;
+
+public class ContadorDePalavras {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entre com um termo a ser pesquisado: ");
+
+        String termo = scanner.nextLine();
+
+        // ??
+
+    }
+
+}
